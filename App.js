ea0f7a296dbb898b1e9b508978a27c0121732d16
Rahman Mohammad Abdur <47378897+abdurrahman16@users.noreply.github.com>
2026-01-05 09:59:35 +0900

    Add files via upload
    updated

--- App.js
+++ App.js
@@ -3,14 +3,59 @@ import './App.css';
 import Book from './Components/Book';
 
 class App extends Component {
+  state={
+    books: [
+      {bookName:'1984', writer:'Babu' }, 
+      {bookName:'2021' ,writer:'Nimny'},
+      {bookName:'2025', writer:'Akter'}
+    ]
+  }
+  changeBookstate = newbookName => {
+    this.setState({
+      books: [
+      {bookName: newbookName, writer:'Babu' }, 
+      {bookName:'325000' ,writer:'Nimnyyyyyy'},
+      {bookName:'20000025', writer:'AAAAAAAkter'}
+    ]
+    });
+
+  }
+
+  changestatewithInput = event =>{
+    this.setState({
+      books: [
+      {bookName: event.target.value, writer:'Babu' }, 
+      {bookName:'325000' ,writer:'Nimnyyyyyy'},
+      {bookName:'20000025', writer:'AAAAAAAkter'}
+    ]
+    });
+  }
   render(){
-    return (
+   
+   
+   const booksState= this.state.books;
+
+   const book = booksState.map(books =>{
+          return (
+            <Book 
+            bookName={books.bookName}
+            writer={books.writer}
+
+            />
+          );
+   });
+
+   
+   
+   
+   return (
 <div className="App">
   <h1>Book List</h1>
-  <Book bookName='1984' writer='Babu' />
-   <Book bookName='2021' writer='Nimny' />
-    <Book bookName='2025' writer='Akter' />
-</div>
+  <button onClick={this.changeBookstate.bind(this,'Change Done!!')}> Change State</button>
+  <input type='text' onChange={this.changestatewithInput}></input>
+     {book}
+   
+   </div>
     );
   }
 }
